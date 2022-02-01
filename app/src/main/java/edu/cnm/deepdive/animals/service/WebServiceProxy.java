b8fccad61088244e5fa3de5b28a2b180b9df3fc8
seanblann <sblann@cnm.edu>
2022-02-01 15:16:33 -0700

    Spinner layout in progress.

--- app/src/main/java/edu/cnm/deepdive/animals/service/WebServiceProxy.java
+++ app/src/main/java/edu/cnm/deepdive/animals/service/WebServiceProxy.java
@@ -14,6 +14,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 
 public interface WebServiceProxy {
+
     @GET(value = "images")
     Call<List<Animal>> getAnimals();
 
