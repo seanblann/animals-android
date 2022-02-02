a157fbb0ebb60f8a19bae2958852fffa849314b7
seanblann <sblann@cnm.edu>
2022-02-02 11:58:20 -0700

    Render image in progress

--- app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.animals.controller;
 
+import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -22,34 +23,39 @@ public class MainActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     animalSelector = findViewById(R.id.animal_selector);
-    new Retriever().start();
   }
 
-  private class Retriever extends Thread {
+  private class RetrieverTask extends AsyncTask<Void, Void, List<Animal>> {
+
 
     @Override
-    public void run() {
+    protected List<Animal> doInBackground(Void... voids) {
       try {
         Response<List<Animal>> response = WebServiceProxy.getInstance()
             .getAnimals()
             .execute();
         if (response.isSuccessful()) {
           Log.d(getClass().getName(), response.body().toString());
-          List<Animal> animals = response.body();
-          runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-              adapter = new ArrayAdapter<>(
-                  MainActivity.this, R.layout.support_simple_spinner_dropdown_item, animals);
-              animalSelector.setAdapter(adapter);
-            }
-          });
+
+          return response.body();
         } else {
           Log.e(getClass().getName(), response.message());
         }
       } catch (IOException e) {
         Log.e(getClass().getName(), e.getMessage(), e);
       }
+      return null;
+    }
+
+    @Override
+    protected void onPostExecute(List<Animal> animals) {
+
+      super.onPostExecute(animals);
+      adapter = new ArrayAdapter<>(
+          MainActivity.this, R.layout.item_animal_spinner, animals);
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+      animalSelector.setAdapter(adapter);
     }
   }
 }
+
