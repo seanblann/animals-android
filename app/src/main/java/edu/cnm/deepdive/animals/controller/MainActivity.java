9339028fb0561ab9133fe6b3227a6ba7c81b84a1
seanblann <sblann@cnm.edu>
2022-02-02 10:02:27 -0700

    quicksave

--- app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals/controller/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.cnm.deepdive.animals.controller;
 
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import edu.cnm.deepdive.animals.R;
@@ -12,10 +14,14 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+  private Spinner animalSelector;
+  private ArrayAdapter adapter;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    animalSelector = findViewById(R.id.animal_selector);
     new Retriever().start();
   }
 
@@ -27,13 +33,22 @@ public class MainActivity extends AppCompatActivity {
         Response<List<Animal>> response = WebServiceProxy.getInstance()
             .getAnimals()
             .execute();
-        if(response.isSuccessful()) {
+        if (response.isSuccessful()) {
           Log.d(getClass().getName(), response.body().toString());
+          List<Animal> animals = response.body();
+          runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+              adapter = new ArrayAdapter<>(
+                  MainActivity.this, R.layout.support_simple_spinner_dropdown_item, animals);
+              animalSelector.setAdapter(adapter);
+            }
+          });
         } else {
           Log.e(getClass().getName(), response.message());
         }
       } catch (IOException e) {
-          Log.e(getClass().getName(), e.getMessage(), e);
+        Log.e(getClass().getName(), e.getMessage(), e);
       }
     }
   }
--- app/src/main/java/edu/cnm/deepdive/animals/model/Animal.java
+++ app/src/main/java/edu/cnm/deepdive/animals/model/Animal.java
@@ -1,8 +1,10 @@
 package edu.cnm.deepdive.animals.model;
 
+import androidx.annotation.NonNull;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 import java.util.UUID;
+import org.jetbrains.annotations.NotNull;
 
 public class Animal {
 
@@ -60,5 +62,11 @@ public class Animal {
 
   public void setDescription(String description) {
     this.description = description;
+  }
+
+  @NonNull
+  @Override
+  public String toString() {
+    return title;
   }
 }
